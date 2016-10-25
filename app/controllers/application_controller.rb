deeca62e4fc8150d50a276f35ff5aab9c4d1ded5
Micah Bales <micahbales@gmail.com>
2016-10-25 12:24:56 -0400

    added model test for review.vote_total

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
   end
+
+  def authenticate_admin!
+    # check first for current user
+    # check to see if user is admin?
+    # allow them to do whatever
+    # else
+  end
 end
